aa086b99cfd12afcfdb2ff6a17f833e58be12f88
Nuker0s <nuker0s2@gmail.com>
2022-11-08 19:14:21 +0100

    Update playermovement.cs

--- eggon/Assets/playermovement.cs
+++ eggon/Assets/playermovement.cs
@@ -5,11 +5,11 @@ using UnityEngine.InputSystem;
 public class playermovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float speed = 5f;
+    public float force = 5f;
     public PlayerInput pinput;
     public InputAction move;
     public Transform orient;
-    public float maxturn;
+    public float maxturnspeed;
     private void Awake()
     {
         move = pinput.actions.FindAction("move");
@@ -34,14 +34,14 @@ public class playermovement : MonoBehaviour
     public void Movement()
     {
         Vector2 dir = move.ReadValue<Vector2>();
-        rb.AddTorque((this.orient.right * dir.y + this.orient.forward* -dir.x)*speed* Time.deltaTime);
+        rb.AddTorque((this.orient.right * dir.y + this.orient.forward* -dir.x)*force* Time.deltaTime);
     }
     public void speedcontroll()
     {
         Vector3 angvel = rb.angularVelocity;
-        if (angvel.magnitude>maxturn)
+        if (angvel.magnitude>maxturnspeed)
         {
-            rb.angularVelocity = angvel.normalized * maxturn;
+            rb.angularVelocity = angvel.normalized * maxturnspeed;
         }
     }
 }
