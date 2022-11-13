cdf8e35d5cf60cd4c0180228d3eaad4fdfe9e7d6
Nuker0s <nuker0s2@gmail.com>
2022-11-13 16:17:23 +0100

    q

--- eggon/Assets/scripts/grapple.cs
+++ eggon/Assets/scripts/grapple.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class grapple : MonoBehaviour
 {
+    public PlayerInput pinput;
+    public InputAction fire;
+    public bool fired = false;
+    public bool catched = false;
+    public Vector3 hitpos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fire = pinput.actions.FindAction("Fire");
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        if (fire.WasPressedThisFrame())
+        {
+            if (!fired)
+            {
+                if (Random.Range(0, 10) >= 5)
+                {
+                    Debug.Log("grabbbed");
+                    fired = true;
+                }
+                else
+                {
+                    Debug.Log("did'thit");
+                }
+            }
+            else
+            {
+                Debug.Log("break");
+                fired = false;
+                hitpos = new Vector3(0,0,0);
+            }
+        }
         
+    }
+    private void OnDrawGizmos()
+    {
+        if (hitpos!= new Vector3(0, 0, 0))
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawSphere(hitpos, 0.3f);
+        }
     }
 }
